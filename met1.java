564773f6d69b040e9cf61168d6da5229bd9e6f51
Sudha <s.sudha19082006@gmail.com>
2025-10-02 19:50:12 +0530

    Create met1.java

---
+++ met1.java
@@ -0,0 +1,9 @@
+public class met1 {
+    public static void sayHello(){ //if we use static in methods we don't need to create objects
+        System.out.println("Hello,World!");
+    }
+    public static void main(String[] args){
+        //met1 obj=new met1();
+        sayHello();
+    }
+}
