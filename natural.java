94b23cbb78416eb06dba02631ebfe15f66be6ed6
Sudha <s.sudha19082006@gmail.com>
2025-10-04 07:54:06 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,10 @@
+public class natural {
+    public static void display(){
+        for(int i=1;i<=10;i=i+1){
+            System.out.println("First 10 natural numbers:"+i);
+        }
+    }
+public static void main(String[] args){
+      display();
+}
+}
